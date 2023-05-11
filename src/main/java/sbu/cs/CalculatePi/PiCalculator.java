1f824eb209ae269d1732006a58211e17a1e827e5
parhammm2014 <parhammm2014@gmail.com>
2023-05-12 02:09:44 +0330

    code works but fail all teat

--- src/main/java/sbu/cs/CalculatePi/PiCalculator.java
+++ src/main/java/sbu/cs/CalculatePi/PiCalculator.java
@@ -1,6 +1,27 @@
 package sbu.cs.CalculatePi;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class PiCalculator {
+    public static int calculateN(int n){
+        n =  n - 2;
+        int a = 2 ;
+        int r = 2 ;
+        int result = a + (r *n );
+        return result;
+    }
+    public static BigDecimal sum ;
+
+    public static synchronized void addTouSum(BigDecimal value){
+        sum = sum.add(value);
+    }
+
+
 
     /**
      * Calculate pi and represent it as a BigDecimal object with the given floating point number (digits after . )
@@ -17,11 +38,28 @@ public class PiCalculator {
 
     public String calculate(int floatingPoint)
     {
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+        sum = new BigDecimal("3");
+        for(int i = 2 ; i < 1000000 ;  i++) {
+            Denominator Task = new Denominator(i, floatingPoint);
+            threadPool.execute(Task);
+        }
+
+        threadPool.shutdown();      // always call before awaitTermination
+
+        try {
+            threadPool.awaitTermination(10000, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        sum = sum.multiply(new BigDecimal(1), new MathContext(1001));
+       sum = sum.multiply(new BigDecimal(1), new MathContext(floatingPoint + 1, RoundingMode.FLOOR));
         // TODO
-        return null;
+//        String Sum = String.valueOf(sum);
+//        String result = Sum.substring(0,( floatingPoint + 2 ) );
+        return sum.toString();
     }
 
     public static void main(String[] args) {
-        // Use the main function to test the code yourself
     }
 }
